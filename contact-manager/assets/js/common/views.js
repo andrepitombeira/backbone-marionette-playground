66bbffc44ac495c948371b4ec33a1398071da2e5
Andre Pitombeira <andrepitombeira@gmail.com>
2015-10-28 17:42:12 +0100

    Passing parameters to the loading view

--- contact-manager/assets/js/common/views.js
+++ contact-manager/assets/js/common/views.js
@@ -2,6 +2,15 @@ ContactManager.module("Common.Views", function(Views, ContactManager, Backbone, 
 	Views.Loading = Marionette.ItemView.extend({
 		template: "#loading-view",
 
+		title: "Loading data",
+		message: "Please wait, data is loading.",
+
+		serializeData: function() {
+			return {
+				title: Marionette.getOption(this, "title"),
+				message: Marionette.getOption(this, "message")
+			}
+		},
 		onShow: function() {
 			var opts = {
 				lines: 13, // The number of lines to draw
