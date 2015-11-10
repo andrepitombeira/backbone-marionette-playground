5aed523620f0426b96c377b7bdc7c78df429b3d3
Andre Pitombeira <andrepitombeira@gmail.com>
2015-11-10 10:08:22 +0100

    Implementing contact editing from a modal window

--- contact-manager/assets/js/apps/contacts/edit/edit_view.js
+++ contact-manager/assets/js/apps/contacts/edit/edit_view.js
@@ -1,17 +1,21 @@
-ContactManager.module("ContactsApp.Edit", function(Edit, ContactManager, Backbone, Marionette, $, _) {
-	Edit.Contact = ContactManager.ContactsApp.Common.Views.Form.extend({
-		initialize: function() {
-			this.title = "Edit " + this.model.get("firstName");
-			this.title = " " + this.model.get("lastName");
-		},
+define(["app", "apps/contacts/common/views"], function(ContactManager, CommonViews) {
+  ContactManager.module("ContactsApp.Edit", function(View, ContactManager, Backbone, Marionette, $, _) {
+  	View.Contact = CommonViews.Form.extend({
+  		initialize: function() {
+  			this.title = "Edit " + this.model.get("firstName");
+  			this.title = " " + this.model.get("lastName");
+  		},
 
-    onRender: function() {
-      if(this.options.generateTitle) {
-        var $title = $("<h1>", { text: this.title });
-        this.$el.prepend($title);
+      onRender: function() {
+        if (this.options.generateTitle) {
+          var $title = $("<h1>", { text: this.title });
+          this.$el.prepend($title);
+        }
+
+        this.$(".js-submit").text("Update contact");
       }
+  	});
+  });
 
-      this.$(".js-submit").text("Update contact");
-    }
-	});
+  return ContactManager.ContactsApp.Edit;
 });
--- contact-manager/assets/js/require_main.js
+++ contact-manager/assets/js/require_main.js
@@ -2,7 +2,9 @@ requirejs.config({
   baseUrl: "assets/js",
   paths: {
     backbone: "vendor/backbone",
+    "backbone.syphon": "vendor/backbone.syphon",
     jquery: "vendor/jquery",
+    "jquery-ui": "vendor/jquery-ui",
     json2: "vendor/json2",
     localstorage: "vendor/backbone.localstorage",
     marionette: "vendor/backbone.marionette",
@@ -18,10 +20,12 @@ requirejs.config({
       deps: ["jquery", "underscore", "json2"],
       exports: "Backbone"
     },
+    "backbone.syphon": ["backbone"],
     marionette: {
       deps: ["backbone"],
       exports: "Marionette"
     },
+    "jquery-ui": ["jquery"],
     localstorage: ["backbone"],
     tpl: ["text"]
   }
