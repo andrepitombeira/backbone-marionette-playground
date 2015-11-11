5e253cbf8b320d3c4c6a9f9b2e5410bf3c33dd30
Andre Pitombeira <andrepitombeira@gmail.com>
2015-11-11 10:43:19 +0100

    Implementing module start/stop according to routes

--- contact-manager/assets/js/apps/contacts/contacts_app_router.js
+++ contact-manager/assets/js/apps/contacts/contacts_app_router.js
@@ -8,57 +8,56 @@ define(["app"], function(ContactManager){
       }
     });
 
-    var executeAction = function(action, arg){
+    var executeAction = function(action, arg) {
       ContactManager.startSubApp("ContactsApp");
       action(arg);
       ContactManager.execute("set:active:header", "contacts");
     };
 
     var API = {
-      listContacts: function(criterion){
-        require(["apps/contacts/list/list_controller"], function(ListController){
+      listContacts: function(criterion) {
+        require(["apps/contacts/list/list_controller"], function(ListController) {
           executeAction(ListController.listContacts, criterion);
         });
       },
 
-      showContact: function(id){
-        require(["apps/contacts/show/show_controller"], function(ShowController){
+      showContact: function(id) {
+        require(["apps/contacts/show/show_controller"], function(ShowController) {
           executeAction(ShowController.showContact, id);
         });
       },
 
-      editContact: function(id){
-        require(["apps/contacts/edit/edit_controller"], function(EditController){
+      editContact: function(id) {
+        require(["apps/contacts/edit/edit_controller"], function(EditController) {
           executeAction(EditController.editContact, id);
         });
       }
     };
 
-    ContactManager.on("contacts:list", function(){
+    ContactManager.on("contacts:list", function() {
       ContactManager.navigate("contacts");
       API.listContacts();
     });
 
-    ContactManager.on("contacts:filter", function(criterion){
-      if(criterion){
+    ContactManager.on("contacts:filter", function(criterion) {
+      if (criterion) {
         ContactManager.navigate("contacts/filter/criterion:" + criterion);
-      }
-      else{
+      } else {
         ContactManager.navigate("contacts");
       }
     });
 
-    ContactManager.on("contact:show", function(id){
+    ContactManager.on("contact:show", function(id) {
       ContactManager.navigate("contacts/" + id);
       API.showContact(id);
     });
 
-    ContactManager.on("contact:edit", function(id){
+    ContactManager.on("contact:edit", function(id) {
       ContactManager.navigate("contacts/" + id + "/edit");
       API.editContact(id);
     });
 
-    ContactManager.Routers.on("start", function(){
+    ContactManager.Routers.on("start", function() {
       new ContactsAppRouter.Router({
         controller: API
       });
