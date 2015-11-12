dd9857d6f3fab4c30772a5e12503f14dcd741a6c
Andre Pitombeira <andrepitombeira@gmail.com>
2015-11-12 13:19:04 +0100

    Initial setup marionette serious progression

---
+++ marionette-serious-progression-server/public/assets/js/app.js
@@ -0,0 +1,32 @@
+var ContactManager = new Marionette.Application();
+
+ContactManager.navigate = function(route,  options){
+  options || (options = {});
+  Backbone.history.navigate(route, options);
+};
+
+ContactManager.getCurrentRoute = function(){
+  return Backbone.history.fragment
+};
+
+ContactManager.on("before:start", function(){
+  var RegionContainer = Marionette.LayoutView.extend({
+    el: "#app-container",
+
+    regions: {
+      main: "#main-region"
+    }
+  });
+
+  ContactManager.regions = new RegionContainer();
+});
+
+ContactManager.on("start", function(){
+  if(Backbone.history){
+    Backbone.history.start();
+
+    if(this.getCurrentRoute() === ""){
+      ContactManager.trigger("contacts:list");
+    }
+  }
+});
