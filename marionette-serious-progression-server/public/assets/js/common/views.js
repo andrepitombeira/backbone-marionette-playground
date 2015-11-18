f562fc376757379c0a10bfd9031e7e028f39c926
Andre Pitombeira <andrepitombeira@gmail.com>
2015-11-18 15:01:51 +0100

    Managing URLs for paginated view

--- marionette-serious-progression-server/public/assets/js/common/views.js
+++ marionette-serious-progression-server/public/assets/js/common/views.js
@@ -48,7 +48,8 @@ ContactManager.module("Common.Views", function(Views, ContactManager, Backbone, 
       var eventsToPropagate = options.propagatedEvents || [];
 
       var controls = new Views.PaginationControls({
-        paginatedCollection: this.collection
+        paginatedCollection: this.collection,
+        urlBase: options.paginatedUrlBase
       });
 
       var listView = new options.mainView({
@@ -80,6 +81,7 @@ ContactManager.module("Common.Views", function(Views, ContactManager, Backbone, 
 
     initialize: function(options) {
       this.paginatedCollection = options.paginatedCollection;
+      this.urlBase = options.urlBase;
       this.listenTo(this.paginatedCollection.parameters, "page:change:after", this.render);
     },
 
@@ -95,7 +97,18 @@ ContactManager.module("Common.Views", function(Views, ContactManager, Backbone, 
     },
 
     serializeData: function() {
-      return this.paginatedCollection.info();
+      var data = this.paginatedCollection.info(),
+          url = this.urlBase,
+          criterion = this.paginatedCollection.parameters.get("criterion");
+
+      if (criterion) {
+        url += "criterion:" + criterion + "+";
+      }
+
+      url += "page:";
+      data.urlBase = url;
+
+      return data;
     }
   });
 });
